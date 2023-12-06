ab7c58c7d8bf71e394d322297969d2d908452e2a
Ryan Goodwin <goodwinryan2@gmail.com>
2023-12-05 22:41:29 -0500

    day 4 part 1

--- 2023/puzzles/day-4/day-4-a.ts
+++ 2023/puzzles/day-4/day-4-a.ts
@@ -1,10 +1,58 @@
+import { CANCELLED } from 'dns';
 import { readData } from '../../common/shared.ts';
 import chalk from 'chalk';
+import { toNumberArray } from '../../common/map.ts';
+import { sumOfArray } from '../../common/math.ts';
+
+interface Card {
+  cardNumber: number
+  winningNumbers: number[]
+  yourNumbers: number[]
+}
 
 export async function day4a(dataPath?: string) {
+  const scores = []
+  const cards: Card[] = []
   const data = await readData(dataPath);
-  return 0;
+  data.forEach((line) => {
+    cards.push(parseLine(line));
+  });
+
+  cards.forEach((card) => {
+    let score = 0;
+    for (const yourNum of card.yourNumbers) {
+      if (card.winningNumbers.includes(yourNum)) {
+        if (score === 0) {
+          score++;
+          continue;
+        }
+        score = score * 2
+      }
+    }
+    scores.push(score);
+  });
+  console.log(scores);
+
+  return sumOfArray(scores);
 }
 
 const answer = await day4a();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+
+function parseLine(line: string): Card {
+  const cardNumber = line.split(":")[0].split(" ")[1];
+  const winningNumbers = line.split(":")[1].split(" | ")[0].trim().split(" ");
+  while (winningNumbers.indexOf("") !== -1) {
+    winningNumbers.splice(winningNumbers.indexOf(""), 1);
+  }
+  const yourNumbers = line.split("| ")[1].replace(/(\r\n|\n|\r)/gm, "").trim().split(" ");
+  while (yourNumbers.indexOf("") !== -1) {
+    yourNumbers.splice(yourNumbers.indexOf(""), 1);
+  }
+
+  return {
+    cardNumber: +cardNumber,
+    winningNumbers: toNumberArray(winningNumbers),
+    yourNumbers: toNumberArray(yourNumbers)
+  };
+}
