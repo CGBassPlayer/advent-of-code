5fbaa919f5f2b7c10da486719675bc5b2f95e335
Tom LeGodais <tomlegodais@gmail.com>
2023-12-12 17:33:19 -0500

    solve day-7-b.ts

--- 2023/puzzles/day-7/day-7-b.ts
+++ 2023/puzzles/day-7/day-7-b.ts
@@ -1,163 +1,116 @@
-import { readData, sumOfArray } from '../../common/index.ts';
+import {readData, sumOfArray} from '../../common/index.ts';
 import chalk from 'chalk';
+
+const STRENGTH_MAPPING = {
+  "J": 1, "2": 2, "3": 3, "4": 4, "5": 5,
+  "6": 6, "7": 7, "8": 8, "9": 9, "T": 10,
+  "Q": 12, "K": 13, "A": 14
+};
 
 interface Card {
   card: string,
-  count: number
+  strength: number
+}
+
+enum HandType {
+  FiveOfAKind,
+  FourOfAKind,
+  FullHouse,
+  ThreeOfAKind,
+  TwoPair,
+  OnePair,
+  HighCard
 }
 
 interface Turn {
   hand: string,
+  type: HandType,
   bid: number,
   cards: Card[]
 }
-
-const CARD_STRENGTH: Card[] = [
-  { card: "A", count: 0 },
-  { card: "K", count: 0 },
-  { card: "Q", count: 0 },
-  { card: "T", count: 0 },
-  { card: "9", count: 0 },
-  { card: "8", count: 0 },
-  { card: "7", count: 0 },
-  { card: "6", count: 0 },
-  { card: "5", count: 0 },
-  { card: "4", count: 0 },
-  { card: "3", count: 0 },
-  { card: "2", count: 0 },
-  { card: "J", count: 0 }
-]
 
 export async function day7b(dataPath?: string) {
   const data = await readData(dataPath);
   let turns = getAllTurns(data);
 
   turns.sort((a, b) => {
-    // occurrences
-    const aJokerCountList = a.cards.map(function (o) {
-      if (o.card === "J") {
-        return o.count;
-      }
-      return;
-    });
-    const bJokerCountList = b.cards.map(function (o) {
-      if (o.card === "J") {
-        return o.count;
-      }
-      return;
-    });
-    const aJokerCount = aJokerCountList[aJokerCountList.length - 1] || 0;
-    const bJokerCount = bJokerCountList[bJokerCountList.length - 1] || 0;
-
-    const aMax: number = Math.max.apply(Math, a.cards.map(function (o) {
-      if (o.card === "J") {
-        return 0; // Joker cards are counted separately, adding them in would double count them
-      }
-      return o.count;
-    }));
-    const bMax: number = Math.max.apply(Math, b.cards.map(function (o) {
-      if (o.card === "J") {
-        return 0; // Joker cards are counted separately, adding them in would double count them
-      }
-      return o.count;
-    }));
-
-    if (aMax + aJokerCount > bMax + bJokerCount) {
-      return 1
-    }
-    if (aMax + aJokerCount < bMax + bJokerCount) {
-      return -1
+    if (a.type !== b.type) {
+      return b.type - a.type;
     }
 
-    // Full House vs 3 of a kind
-    if (aMax + aJokerCount === 3 && bMax + bJokerCount === 3) {
-      const aCounts = a.cards.map(function (o) { return o.count });
-      const bCounts = b.cards.map(function (o) { return o.count });
-
-      if (aCounts.includes(2) && !bCounts.includes(2)) {
-        return 1;
-      }
-      if (!aCounts.includes(2) && bCounts.includes(2)) {
-        return -1;
-      }
+    for (let i = 0; i < a.cards.length; i++) {
+      if (a.cards[i].strength !== b.cards[i].strength)
+        return a.cards[i].strength - b.cards[i].strength;
     }
 
-    // 2 Pairs vs 2 of a kind
-    if (aMax + aJokerCount === 2 && bMax + bJokerCount === 2) {
-      const aCounts = a.cards.map(function (o) { return o.count });
-      const bCounts = b.cards.map(function (o) { return o.count });
-
-      if (aCounts.indexOf(2) !== aCounts.lastIndexOf(2) && bCounts.indexOf(2) === bCounts.lastIndexOf(2)) {
-        return 1;
-      }
-      if (aCounts.indexOf(2) === aCounts.lastIndexOf(2) && bCounts.indexOf(2) !== bCounts.lastIndexOf(2)) {
-        return -1;
-      }
-    }
-
-    // Card value check
-    let cardIdx = 0
-    while (cardIdx < a.hand.length) {
-      const aCardStrength = CARD_STRENGTH.findIndex(item => item.card === a.hand[cardIdx]);
-      const bCardStrength = CARD_STRENGTH.findIndex(item => item.card === b.hand[cardIdx]);
-      if (aCardStrength < bCardStrength) {
-        return 1;
-      }
-      if (aCardStrength > bCardStrength) {
-        return -1;
-      }
-      cardIdx++;
-    }
     return 0;
   });
 
-  console.table(turns);
-
-  const totalWinnigs: number[] = []
+  const totalWinnings: number[] = []
   turns.forEach((turn, rank) => {
-    totalWinnigs.push(turn.bid * (rank + 1));
+    totalWinnings.push(turn.bid * (rank + 1));
   });
 
-  return sumOfArray(totalWinnigs);
+  return sumOfArray(totalWinnings);
 }
 
 const answer = await day7b();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
-// 248731027 too low
-// 248362756
-
 
 function getAllTurns(data: string[]): Turn[] {
   const turns: Turn[] = [];
   for (const line of data) {
-    let counts: Card[] = [
-      { card: "A", count: 0 },
-      { card: "K", count: 0 },
-      { card: "Q", count: 0 },
-      { card: "T", count: 0 },
-      { card: "9", count: 0 },
-      { card: "8", count: 0 },
-      { card: "7", count: 0 },
-      { card: "6", count: 0 },
-      { card: "5", count: 0 },
-      { card: "4", count: 0 },
-      { card: "3", count: 0 },
-      { card: "2", count: 0 },
-      { card: "J", count: 0 }
-    ];
+    let cards: Card[] = [];
+
     const [hand, bid] = line.split(" ");
-    const cards = hand.split('');
-    for (const cs of counts) {
-      for (const card of cards) {
-        if (cs.card === card) {
-          cs.count++;
-        }
+    for (const card of hand.split('')) {
+      cards.push({card: card, strength: STRENGTH_MAPPING[card]});
+    }
+
+    let jokerCount = 0;
+    let frequency = new Map<string, number>();
+
+    for (const card of cards) {
+      if (card.card === "J") jokerCount++;
+      else frequency.set(card.card, (frequency.get(card.card) || 0) + 1);
+    }
+
+    for (let j = 0; j < jokerCount; j++) {
+      const bestUse = [...frequency.entries()].reduce((max, current) => {
+        return max === null || current[1] > max[1] ? current : max;
+      }, null);
+
+      if (bestUse) {
+        const [key, _] = bestUse;
+
+        frequency.set(key, frequency.get(key) + 1);
+      } else {
+        frequency["*"] = 1;
       }
     }
+
+    let pairs = 0, triplets = 0, quads = 0, quints = 0;
+    for (const [_, val] of frequency) {
+      if (val === 2) pairs++;
+      else if (val == 3) triplets++;
+      else if (val == 4) quads++;
+      else if (val == 5) quints++;
+    }
+
+    let handType: HandType;
+    if (quints === 1) handType = HandType.FiveOfAKind;
+    else if (quads === 1) handType = HandType.FourOfAKind;
+    else if (triplets === 1 && pairs === 1) handType = HandType.FullHouse;
+    else if (triplets === 1) handType = HandType.ThreeOfAKind;
+    else if (pairs === 2) handType = HandType.TwoPair;
+    else if (pairs === 1) handType = HandType.OnePair;
+    else handType = HandType.HighCard;
+
     turns.push({
       hand: hand,
+      type: handType,
       bid: parseInt(bid),
-      cards: counts
+      cards: cards
     })
   }
 
