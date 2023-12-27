b8f5bd5ab9fec8f390fec8f8b3a67258cba76407
Ryan Goodwin <goodwinryan2@gmail.com>
2023-12-27 15:49:52 -0500

    WIP day 10

--- 2023/puzzles/day-10/day-10-a.ts
+++ 2023/puzzles/day-10/day-10-a.ts
@@ -1,28 +1,87 @@
 import { readData } from '../../common/shared.ts';
 import chalk from 'chalk';
 
+interface Connection {
+  x: number
+  y: number
+}
+
+interface Tile {
+  symbol: string,
+  connections: Connection[]
+}
+
+const TILES = {
+  NS: {
+    symbol: "|",
+    connections: [
+      { x: 0, y: 1 },
+      { x: 0, y: -1 }
+    ]
+  },
+  EW: {
+    symbol: "-",
+    connections: [
+      { x: 1, y: 0 },
+      { x: -1, y: 0 }
+    ]
+  },
+  NE: {
+    symbol: "L",
+    connections: [
+      { x: 1, y: 0 },
+      { x: 0, y: 1 }
+    ]
+  },
+  NW: {
+    symbol: "J",
+    connections: [
+      { x: 1, y: 0 },
+      { x: 0, y: -1 }
+    ]
+  },
+  SE: {
+    symbol: "F",
+    connections: [
+      { x: -1, y: 0 },
+      { x: 0, y: 1 }
+    ]
+  },
+  SW: {
+    symbol: "7",
+    connections: [
+      { x: -1, y: 0 },
+      { x: 0, y: -1 }
+    ]
+  },
+  GND: {
+    symbol: ".",
+    connections: [
+      { x: 0, y: 0 },
+    ]
+  },
+  START: {
+    symbol: "S",
+    connections: [
+      { x: 1, y: 0 },
+      { x: -1, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: -1 }
+    ]
+  }
+}
+
 interface Position {
   tile: Tile,
   x: number,
   y: number
 }
 
-enum Tile {
-  NS = "|",
-  EW = "-",
-  NE = "L",
-  NW = "J",
-  SW = "7",
-  SE = "F",
-  GND = ".",
-  START = "S"
-}
-
 export async function day10a(dataPath?: string) {
   const data = await readData(dataPath);
   const tiles: Tile[][] = parseTiles(data);
-  console.table(tiles);
-  console.log(findStartLocation(tiles));
+  const startTile = findStartLocation(tiles);
+  console.log(startTile);
   return 0;
 }
 
@@ -34,36 +93,83 @@ function parseTiles(data: string[]): Tile[][] {
   for (const line of data) {
     tiles.push([]);
     for (const char of line) {
-      tiles[data.indexOf(line)].push(<Tile> char)
+      switch (char) {
+        case TILES.NS.symbol:
+          tiles[data.indexOf(line)].push(TILES.NS);
+          break;
+        case TILES.EW.symbol:
+          tiles[data.indexOf(line)].push(TILES.EW);
+          break;
+        case TILES.NE.symbol:
+          tiles[data.indexOf(line)].push(TILES.NE);
+          break;
+        case TILES.NW.symbol:
+          tiles[data.indexOf(line)].push(TILES.NW);
+          break;
+        case TILES.SE.symbol:
+          tiles[data.indexOf(line)].push(TILES.SE);
+          break;
+        case TILES.SW.symbol:
+          tiles[data.indexOf(line)].push(TILES.SW);
+          break;
+        case TILES.GND.symbol:
+          tiles[data.indexOf(line)].push(TILES.GND);
+          break;
+        case TILES.START.symbol:
+          tiles[data.indexOf(line)].push(TILES.START);
+          break;
+        default:
+          console.log(chalk.redBright("Unknown Tile Type provided. Failed to parse data"));
+          process.exit(1);
+      }
+
     }
   }
   return tiles;
 }
 
-function findStartLocation(tiles: Tile[][]): Position {
-  for (const row of tiles) {
-    if (row.indexOf(Tile.START) !== -1) {
+function findStartLocation(map: Tile[][]): Position {
+  for (const row of map) {
+    if (row.indexOf(TILES.START) !== -1) {
       return {
-        tile: Tile.START,
-        x: tiles.indexOf(row),
-        y: row.indexOf(Tile.START)
+        tile: TILES.START,
+        x: map.indexOf(row),
+        y: row.indexOf(TILES.START)
       }
     }
   }
+  console.log(chalk.red("Start Position not found!"))
   return {
-    tile: Tile.START,
+    tile: TILES.START,
     x: -1,
     y: -1
   }
 }
 
-function pipeAttachemnts(map: Tile[][], tile: Position): Position[] {
+function findattachesPipes(map: Tile[][], tile: Position): Position[] {
   const attachedPipes: Position[] = [];
-  switch (tile.x) {
-    case 0:
-      break;
-    case map.length - 1:
-      break;
+
+  for (const ct of tile.tile.connections) {
+    if (tile.x + ct.x < 0 || tile.x + ct.x >= map.length) {
+      continue;
+    }
+    if (tile.y + ct.y < 0 || tile.y + ct.y >= map[0].length) {
+      continue;
+    }
+    if (map[tile.x + ct.x][tile.y + ct.y] == TILES.GND) {
+      continue;
+    }
+
+    const nextTile: Position = {
+      tile: map[tile.x + ct.x][tile.y + ct.y],
+      x: tile.x + ct.x,
+      y: tile.y + ct.y
+    }
+
+    if (tile.tile.connections.includes({ x: nextTile.x, y: nextTile.y })) {
+      attachedPipes.push(nextTile);
+    }
   }
+
   return attachedPipes;
 }
